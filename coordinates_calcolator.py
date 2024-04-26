b9418efe7b6092c9b005781e1730552e82b2cc41
Lorenzo Battisti <114929610+LorenzoBattistiRomaTre@users.noreply.github.com>
2024-04-26 11:58:35 +0200

    eureka!

--- coordinates_calcolator.py
+++ coordinates_calcolator.py
@@ -6,3 +6,10 @@ def lat_lon_to_tile(lat, lon, zoom):
     xtile = int((lon + 180.0) / 360.0 * n)
     ytile = int((1.0 - math.log(math.tan(lat_rad) + (1 / math.cos(lat_rad))) / math.pi) / 2.0 * n)
     return xtile, ytile
+
+
+def lat_lon_to_tile_cil(lat, lon, zoom):
+    n = 2 ** zoom
+    x = (lon + 180.0) / 360.0 * n
+    y = (1.0 - math.sin(math.radians(lat))) / 2.0 * n
+    return int(x), int(y)
